783e573f71048ebc81d09f93458925f29f472dc9
Huali Dong <huali.dong@Hualis-MacBook-Pro.local>
2022-05-24 17:12:30 +0800

    [huali] feat: refeactor time

--- src/codeReview.js
+++ src/codeReview.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule')
 const axios = require('axios')
 const codeReviewCorn = () => {
-    schedule.scheduleJob({hour: 17, minute: 30, dayOfWeek: [1, 2, 3, 4, 5, 6, 0]}, () => {
+    schedule.scheduleJob({hour: 17, minute: 30, dayOfWeek: [1, 2, 3, 4, 5]}, () => {
         axios.post(process.env.WECOM_ROBOT_URL, {
             msgtype: "text", text: {
                 content: "😜dev们准备一下，该codeReview啦"
